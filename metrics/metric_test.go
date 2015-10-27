7f59ed6bc09d60936ef6bfeb2ccb7815f27b3497
Dan Carley <dan.carley@gmail.com>
2015-10-27 12:13:03 +0000

    Add prefixJob option
    Which prepend the CloudFoundry job name (e.g. `runner_z1`) and job index
    (e.g. `0`) to every metric name. We want this in order to debug issues with
    particular servers, for example if Doppler is sending messages on one side
    but they aren't all being received on the other end.
    
    The way that this option is passed through is sub-optimal. We did consider
    some other options like whether we could change the prefix of the statsd
    client on the fly, but it wasn't possible/efficient. The codebase could
    benefit from some refactoring, to reduce duplication.
    
    To use this you will need to run something like:
    
        cf set-env graphite-nozzle PREFIX_JOB true

--- metrics/metric_test.go
+++ metrics/metric_test.go
@@ -115,53 +115,105 @@ var _ = Describe("Metric", func() {
 		})
 
 		Context("with a PrecisionTimingMetric", func() {
-			It("sends the Metric to StatsD with time.Duration precision", func() {
-				metric := NewPrecisionTimingMetric("http.responsetimes.api_10_244_0_34_xip_io", 50*time.Millisecond)
-				metric.Send(fakeStatsdClient)
+			Context("without prefix", func() {
+				It("sends the Metric to StatsD with time.Duration precision", func() {
+					metric := NewPrecisionTimingMetric("http.responsetimes.api_10_244_0_34_xip_io", 50*time.Millisecond)
+					metric.Send(fakeStatsdClient, "")
 
-				Expect(fakeStatsdClient.precisionTimingCalled).To(BeTrue())
-				Expect(fakeStatsdClient.stat).To(Equal("http.responsetimes.api_10_244_0_34_xip_io"))
-				Expect(fakeStatsdClient.precisionTimingValue).To(Equal(50 * time.Millisecond))
+					Expect(fakeStatsdClient.precisionTimingCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("http.responsetimes.api_10_244_0_34_xip_io"))
+					Expect(fakeStatsdClient.precisionTimingValue).To(Equal(50 * time.Millisecond))
+				})
+			})
+
+			Context("with prefix", func() {
+				It("sends the Metric to StatsD with time.Duration precision", func() {
+					metric := NewPrecisionTimingMetric("http.responsetimes.api_10_244_0_34_xip_io", 50*time.Millisecond)
+					metric.Send(fakeStatsdClient, "")
+
+					Expect(fakeStatsdClient.precisionTimingCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("http.responsetimes.api_10_244_0_34_xip_io"))
+					Expect(fakeStatsdClient.precisionTimingValue).To(Equal(50 * time.Millisecond))
+				})
 			})
 		})
 
 		Context("with a CounterMetric", func() {
-			It("sends the Metric to StatsD with int64 precision", func() {
-				metric := NewCounterMetric("http.statuscodes.api_10_244_0_34_xip_io.200", 1)
-				metric.Send(fakeStatsdClient)
+			Context("without prefix", func() {
+				It("sends the Metric to StatsD with int64 precision", func() {
+					metric := NewCounterMetric("http.statuscodes.api_10_244_0_34_xip_io.200", 1)
+					metric.Send(fakeStatsdClient, "")
 
-				Expect(fakeStatsdClient.incrCalled).To(BeTrue())
-				Expect(fakeStatsdClient.stat).To(Equal("http.statuscodes.api_10_244_0_34_xip_io.200"))
-				Expect(fakeStatsdClient.value).To(Equal(int64(1)))
+					Expect(fakeStatsdClient.incrCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("http.statuscodes.api_10_244_0_34_xip_io.200"))
+					Expect(fakeStatsdClient.value).To(Equal(int64(1)))
+				})
+			})
+
+			Context("with prefix", func() {
+				It("sends the Metric to StatsD with int64 precision", func() {
+					metric := NewCounterMetric("http.statuscodes.api_10_244_0_34_xip_io.200", 1)
+					metric.Send(fakeStatsdClient, "myjob_z1.0")
+
+					Expect(fakeStatsdClient.incrCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("myjob_z1.0.http.statuscodes.api_10_244_0_34_xip_io.200"))
+					Expect(fakeStatsdClient.value).To(Equal(int64(1)))
+				})
 			})
 		})
 
 		Context("with a GaugeMetric", func() {
-			It("sends the Metric to StatsD with int64 precision", func() {
-				metric := NewGaugeMetric("router__0.numCPUS", 4)
-				metric.Send(fakeStatsdClient)
+			Context("without prefix", func() {
+				It("sends the Metric to StatsD with int64 precision", func() {
+					metric := NewGaugeMetric("router__0.numCPUS", 4)
+					metric.Send(fakeStatsdClient, "")
 
-				Expect(fakeStatsdClient.gaugeCalled).To(BeTrue())
-				Expect(fakeStatsdClient.stat).To(Equal("router__0.numCPUS"))
-				Expect(fakeStatsdClient.value).To(Equal(int64(4)))
+					Expect(fakeStatsdClient.gaugeCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("router__0.numCPUS"))
+					Expect(fakeStatsdClient.value).To(Equal(int64(4)))
+				})
+			})
+
+			Context("with prefix", func() {
+				It("sends the Metric to StatsD with int64 precision", func() {
+					metric := NewGaugeMetric("router__0.numCPUS", 4)
+					metric.Send(fakeStatsdClient, "myjob_z1.0")
+
+					Expect(fakeStatsdClient.gaugeCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("myjob_z1.0.router__0.numCPUS"))
+					Expect(fakeStatsdClient.value).To(Equal(int64(4)))
+				})
 			})
 		})
 
 		Context("with an FGaugeMetric", func() {
-			It("sends the Metric to StatsD with float64 precision", func() {
-				metric := NewFGaugeMetric("router__0.numCPUS", 4)
-				metric.Send(fakeStatsdClient)
+			Context("without prefix", func() {
+				It("sends the Metric to StatsD with float64 precision", func() {
+					metric := NewFGaugeMetric("router__0.numCPUS", 4)
+					metric.Send(fakeStatsdClient, "")
 
-				Expect(fakeStatsdClient.fGaugeCalled).To(BeTrue())
-				Expect(fakeStatsdClient.stat).To(Equal("router__0.numCPUS"))
-				Expect(fakeStatsdClient.fValue).To(Equal(float64(4)))
+					Expect(fakeStatsdClient.fGaugeCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("router__0.numCPUS"))
+					Expect(fakeStatsdClient.fValue).To(Equal(float64(4)))
+				})
+			})
+
+			Context("with prefix", func() {
+				It("sends the Metric to StatsD with float64 precision", func() {
+					metric := NewFGaugeMetric("router__0.numCPUS", 4)
+					metric.Send(fakeStatsdClient, "myjob_z1.0")
+
+					Expect(fakeStatsdClient.fGaugeCalled).To(BeTrue())
+					Expect(fakeStatsdClient.stat).To(Equal("myjob_z1.0.router__0.numCPUS"))
+					Expect(fakeStatsdClient.fValue).To(Equal(float64(4)))
+				})
 			})
 		})
 
 		Context("when the StatsdClient doesn't return an error", func() {
 			It("doesn't return an error", func() {
 				metric := NewGaugeMetric("router__0.numCPUS", 4)
-				err := metric.Send(fakeStatsdClient)
+				err := metric.Send(fakeStatsdClient, "")
 
 				Expect(err).ToNot(HaveOccurred())
 			})
@@ -170,7 +222,7 @@ var _ = Describe("Metric", func() {
 		Context("when the StatsdClient returns an error", func() {
 			It("returns the error", func() {
 				metric := NewFGaugeMetric("router__0.numCPUS", 4)
-				err := metric.Send(fakeStatsdClient)
+				err := metric.Send(fakeStatsdClient, "")
 
 				Expect(err).To(MatchError(errors.New("StatsdClientSendError")))
 			})
