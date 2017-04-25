7825e381c72f0f52d874c5867a28ec8b6e44a9c0
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-04-25 22:34:11 +0100

    improve logging mesages

--- metrics/metrics_utils.go
+++ metrics/metrics_utils.go
@@ -1,26 +1,29 @@
 package metrics
 
 import (
-	"fmt"
+	"errors"
+            "fmt"
 	"time"
 
             "github.com/pivotal-cf/graphite-nozzle/logging"
 )
 
 func Retry(attempts int, sleep time.Duration, op func() error) (err error) {
-	for i := 0; ; i++ {
-		err = op()
-		if err == nil {
-			return
-		}
+    for i := 0; ; i++ {
+        err = op()
+        if err == nil {
+             logging.LogStd(fmt.Sprintf("succeeded after %d attempts", i+1))
+            return
+        }
 
-		if i >= (attempts - 1) {
-			break
-		}
+        if i >= (attempts - 1) {
+	break
+        }
 
-		time.Sleep(sleep)
+        time.Sleep(sleep)
 
-		logging.LogError("retrying after error:", err)
-	}
-	return fmt.Errorf("after %d attempts, last error: %s", attempts, err)
+        logging.LogError(fmt.Sprintf("failed retrying, attempt %d of %d", i+1, attempts), err)
+    }
+
+    return errors.New(fmt.Sprintf("gave up after %d attempts", attempts))
 }
