4536323bf61d94231770984b76b582f319b5844e
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-04-25 21:31:37 +0100

    refactor main package

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"os"
-
 	"github.com/cloudfoundry/noaa/consumer"
 	"github.com/cloudfoundry/sonde-go/events"
+	"github.com/pivotal-cf/graphite-nozzle/logging"
 	"github.com/pivotal-cf/graphite-nozzle/metrics"
 	"github.com/pivotal-cf/graphite-nozzle/processors"
 	"github.com/pivotal-cf/graphite-nozzle/token"
-	"github.com/quipo/statsd"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"net"
+	"os"
 )
 
 var (
@@ -23,7 +23,7 @@ var (
 	subscriptionId    = kingpin.Flag("subscription-id", "Id for the subscription.").Default("firehose").OverrideDefaultFromEnvar("SUBSCRIPTION_ID").String()
 	statsdEndpoint    = kingpin.Flag("statsd-endpoint", "Statsd endpoint").Default("10.244.11.2:8125").OverrideDefaultFromEnvar("STATSD_ENDPOINT").String()
 	statsdPrefix      = kingpin.Flag("statsd-prefix", "Statsd prefix").Default("mycf.").OverrideDefaultFromEnvar("STATSD_PREFIX").String()
-	statsdProtocol      = kingpin.Flag("statsd-protocol", "Statsd protocol, either udp or tcp").Default("udp").OverrideDefaultFromEnvar("STATSD_PROTOCOL").String()
+	statsdProtocol    = kingpin.Flag("statsd-protocol", "Statsd protocol, either udp or tcp").Default("udp").OverrideDefaultFromEnvar("STATSD_PROTOCOL").String()
 	prefixJob         = kingpin.Flag("prefix-job", "Prefix metric names with job.index").Default("false").OverrideDefaultFromEnvar("PREFIX_JOB").Bool()
 	username          = kingpin.Flag("username", "Firehose username.").Default("admin").OverrideDefaultFromEnvar("FIREHOSE_USERNAME").String()
 	password          = kingpin.Flag("password", "Firehose password.").Default("admin").OverrideDefaultFromEnvar("FIREHOSE_PASSWORD").String()
@@ -31,12 +31,39 @@ var (
 	debug             = kingpin.Flag("debug", "Enable debug mode. This disables forwarding to statsd and prints to stdout").Default("false").OverrideDefaultFromEnvar("DEBUG").Bool()
 )
 
+func processMetric(msg *events.Envelope, metric metrics.Metric, sender metrics.StatsdClient) {
+	var prefix string
+	if *prefixJob {
+		prefix = msg.GetJob() + "." + msg.GetIndex()
+	}
+	send_err := metric.Send(sender, prefix)
+
+	switch i := send_err.(type) {
+	default:
+	case nil:
+	case error:
+		logging.LogError(fmt.Sprintf("Error while sending metric %v :", metric), send_err)
+		//if the error is generated during a write operation and the protocol is tcp
+		//try to reconnect
+		if net_err, ok := i.(*net.OpError); ok {
+			if net_err.Net == "tcp" {
+				sender.Close()
+				rec_err := sender.Reconnect()
+				if rec_err != nil {
+					logging.LogError("Error while re-connecting to statsd", rec_err)
+					os.Exit(-1)
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	kingpin.Parse()
 
 	err := ValidateStatsdProtocol(*statsdProtocol)
 	if err != nil {
-		fmt.Println(err)
+		logging.LogError("Error while validating statsd protocol", err)
 		os.Exit(-1)
 	}
 
@@ -49,7 +76,7 @@ func main() {
 
 	authToken, err := tokenFetcher.FetchAuthToken()
 	if err != nil {
-		fmt.Println(err)
+		logging.LogError("Error while fetching auth token", err)
 		os.Exit(-1)
 	}
 
@@ -61,20 +88,25 @@ func main() {
 	containerMetricProcessor := processors.NewContainerMetricProcessor()
 	counterProcessor := processors.NewCounterProcessor()
 
-	//Initialising statsd sender
-	sender := statsd.NewStatsdClient(*statsdEndpoint, *statsdPrefix)
-
-	switch 	statsdProtocol := *statsdProtocol; statsdProtocol {
-		case "udp":
-			err = sender.CreateSocket()
-			fmt.Println("Using udp protocol for statsd")
-		case "tcp":
-			err = sender.CreateTCPSocket()
-			fmt.Println("Using tcp protocol for statsd")
+	//configuration for statsd sender
+	clientConf := map[string]string{
+		"protocol": *statsdProtocol,
+		"endpoint": *statsdEndpoint,
+		"prefix":   *statsdPrefix,
 	}
 
+	//initialising statsd sender
+	logging.LogStd(fmt.Sprintf("Using %s protocol for statsd", *statsdProtocol), true)
+
+	sender, err := metrics.CreateClient(clientConf)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while connecting to statsd: %v\n", err.Error())
+		logging.LogError("Error while connecting to statsd", err)
+		os.Exit(-1)
+	}
+	//connetcting to the statsd server
+	err = sender.Connect()
+	if err != nil {
+		logging.LogError("Error while connecting to statsd", err)
 		os.Exit(-1)
 	}
 
@@ -85,7 +117,7 @@ func main() {
 
 	go func() {
 		for err := range errorChan {
-			fmt.Fprintf(os.Stderr, "%v\n", err.Error())
+			logging.LogError("", err)
 		}
 	}()
 
@@ -108,23 +140,19 @@ func main() {
 		}
 
 		if proc_err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", proc_err.Error())
+			logging.LogError("", proc_err)
 			continue
 		}
 
 		if !*debug {
 			if len(processedMetrics) > 0 {
 				for _, metric := range processedMetrics {
-					var prefix string
-					if *prefixJob {
-						prefix = msg.GetJob() + "." + msg.GetIndex()
-					}
-					metric.Send(sender, prefix)
+					processMetric(msg, metric, sender)
 				}
 			}
 		} else {
 			for _, msg := range processedMetrics {
-				fmt.Println(msg)
+				logging.LogStd(fmt.Sprintf("%v", msg), true)
 			}
 		}
 
