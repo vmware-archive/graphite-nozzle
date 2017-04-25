74494c56fe1a6155a01d10dc9a17a74bf9500087
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-04-25 22:31:58 +0100

    update logging library

--- logging/logging.go
+++ logging/logging.go
@@ -6,28 +6,25 @@ import (
 	"time"
 )
 
-func LogStd(message string, force bool) {
-	Log(message, force, false, nil)
+func LogStd(message string) {
+	Log(message, false, nil)
 }
 
 func LogError(message string, errMsg interface{}) {
-	Log(message, false, true, errMsg)
+	Log(message, true, errMsg)
 }
 
-func Log(message string, force bool, isError bool, err interface{}) {
+func Log(message string, isError bool, err interface{}) {
 
-	if force || isError {
+	writer := os.Stdout
+	var formattedMessage string
 
-		writer := os.Stdout
-		var formattedMessage string
-
-		if isError {
-			writer = os.Stderr
-			formattedMessage = fmt.Sprintf("[%s] Exception occurred! Message: %s Details: %v", time.Now().String(), message, err)
-		} else {
-			formattedMessage = fmt.Sprintf("[%s] %s", time.Now().String(), message)
-		}
-
-		fmt.Fprintln(writer, formattedMessage)
+	if isError {
+		writer = os.Stderr
+		formattedMessage = fmt.Sprintf("[%s] Exception occurred! | Message: %s | Details: %v", time.Now().String(), message, err)
+	} else {
+		formattedMessage = fmt.Sprintf("[%s] %s", time.Now().String(), message)
 	}
+
+	fmt.Fprintln(writer, formattedMessage)
 }
